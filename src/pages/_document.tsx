9e56da8ee88f380dd1c93cf06fc801b7c7e641f6
shaynlink <guemmoudrayane03@gmail.com>
2023-02-17 15:42:29 +0100

    not needed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { CSPGenerator } from '@/utils'
-
-const generateCSP = CSPGenerator();
 
 export default function Document() {
-  const [, nonce] = generateCSP();
 
   return (
     <Html lang="en">
-      <Head nonce={nonce}>
-        <meta property='csp-nonce' content={nonce} />
+      <Head>
+        <meta property='csp-nonce' />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" />
       </Head>
       <body>
         <Main />
-        <NextScript nonce={nonce} />
+        <NextScript />
       </body>
     </Html>
   )
--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,5 @@
-import genToken from '@shaynlink/gentoken';
-import securityHeaders from '../securityHeaders';
-
 export function classNames(...args: string[]): string {
     return args.join(' ');
 }
 
 export const classNameIcon: string = 'material-symbols-outlined';
-
-export function CSPGenerator(): () => [string, string] {
-    const generator: IterableIterator<string> = genToken();
-
-    // generateCsp function
-    return function generateCSP(): [string, string] {
-        let csp = securityHeaders.find(({key}) => key == 'Content-Security-Policy')?.key;
-        if (!csp) {
-            csp = ''
-        }
-
-        return [
-            csp,
-            generator.next().value
-        ];
-    }
-}
